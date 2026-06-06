783381fa1207ff78e541a109b2e82a0b75dba940
Seunghoon Lim <woo121168@gmail.com>
2026-06-06 17:56:52 +0900

    feat: refresh user profile after token exchange

--- src/apis/userApi.ts
+++ src/apis/userApi.ts
@@ -1,14 +1,10 @@
-import axios from "axios"
 import { SPOTIFY_BASE_URL } from "../configs/commonConfig"
 import { User } from "../models/user"
+import api from "../utils/api"
 
 export const getCurrentUserProfile=async():Promise<User>=>{
     try{
-        const response = await axios.get(`${SPOTIFY_BASE_URL}/me`,{
-            headers:{
-                Authorization:`Bearer ${localStorage.getItem("spotify_access_token")}`
-            }
-        })
+        const response = await api.get(`/me`)
         return response.data
     }catch(error){
         throw new Error("fail to fetch user profile")
--- src/hooks/useExchangeToken.ts
+++ src/hooks/useExchangeToken.ts
@@ -1,12 +1,16 @@
-import { useMutation } from "@tanstack/react-query"
+import { useMutation, useQueryClient } from "@tanstack/react-query"
 import { exchangeToken } from "../apis/authApi"
 import { ExchangeTokenResponse } from "../models/auth";
 
 const useExchangeToken =()=>{
+    const queryClient = useQueryClient()
     return useMutation<ExchangeTokenResponse, Error, {code:string, codeVerifier:string}>({
         mutationFn:({code,codeVerifier})=>exchangeToken(code,codeVerifier),
         onSuccess:(data)=>{
             localStorage.setItem('access_token', data.access_token);
+            queryClient.invalidateQueries({
+                queryKey:["current-user-profile"]
+            })
         }
 
     })
